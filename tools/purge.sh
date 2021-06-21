9ec70cf3b141d3a05c845043c75e3fd3579a3da5
Tor Andersson <tor@ccxvii.net>
2021-06-21 11:12:03 +0000

    Purge stale games after two weeks.

--- tools/purge.sh
+++ tools/purge.sh
@@ -3,4 +3,4 @@
 # Clean out stale games from the database.
 
 sqlite3 db "DELETE FROM games WHERE status = 0 AND mtime < datetime('now', '-7 days')"
-sqlite3 db "UPDATE games SET status = 3 WHERE status = 1 AND mtime < datetime('now', '-28 days')"
+sqlite3 db "UPDATE games SET status = 3 WHERE status = 1 AND mtime < datetime('now', '-14 days')"
