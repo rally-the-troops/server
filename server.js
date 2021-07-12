00bc6cae6aa956d108b4a9ef956c4781a46253ba
Tor Andersson <tor@ccxvii.net>
2021-07-12 22:13:29 +0200

    Disable same-site on cookies to allow redirects from gmail, etc.

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.use(express_session({
 	rolling: true,
 	saveUninitialized: false,
 	store: session_store,
-	cookie: { maxAge: 7 * 24 * 60 * 60 * 1000, sameSite: 'strict' }
+	cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 }
 }));
 app.use(connect_flash());
 
